4a9c27e3ea159388a0c7a82ff30c92e913a680fe
coding-partner <101594309+coding-partner@users.noreply.github.com>
2022-03-14 22:55:08 +0330

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,49 @@
+// let counter = 0 ;
+// function count(){
+//     counter++
+//     document.querySelector('h1').innerHTML= counter
+//     if(counter % 10 === 0){
+//     alert(`Count is now ${counter}`)}
+// }
+// document.addEventListener('DOMContentLoaded', function(){
+//     document.querySelector('button').onclick = count
+// })
+
+// let counter = 0;
+            
+// function count() {
+//     counter++;
+//     document.querySelector('h1').innerHTML = counter;
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.querySelector('button').onclick = count;
+
+//     setInterval(count, 1000);
+// });
+
+
+// Check if there is already a vlaue in local storage
+if (!localStorage.getItem('counter')) {
+
+    // If not, set the counter to 0 in local storage
+    localStorage.setItem('counter', 0);
+}
+            
+function count() {
+    // Retrieve counter value from local storage
+    let counter = localStorage.getItem('counter');
+
+    // update counter
+    counter++;
+    document.querySelector('h1').innerHTML = counter;
+
+    // Store counter in local storage
+    localStorage.setItem('counter', counter);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Set heading to the current value inside local storage
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
+    document.querySelector('button').onclick = count;
+});
